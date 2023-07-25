e5c4eb580a0df378176c7ab9fb82f494e036e6b4
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-07-25 16:38:51 -0700

    Add catppuccin available colorscheme comments

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,6 +3,7 @@ local colorscheme = 'dracula'
 -- monokai_pro
 -- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
 -- dracula, dracula-soft
+-- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 
 local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not is_ok then
