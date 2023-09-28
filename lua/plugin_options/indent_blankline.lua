6da14fb68f8cb9b2a9b2b2577a186ffe02d60d8b
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-09-28 11:12:23 -0700

    Update indent_blankline.lua

--- lua/plugin_options/indent_blankline.lua
+++ lua/plugin_options/indent_blankline.lua
@@ -5,5 +5,13 @@ end
 
 configs.setup {}
 
+    -- -- vim.opt.list = true
+    -- -- vim.opt.listchars:append "eol:↴"
+    -- --
+    -- require("indent_blankline").setup {
+    --     show_current_context = true,
+    --     -- show_end_of_line = true,
+    -- }
+
 vim.keymap.set('n', '<leader>ibld', ':IndentBlanklineDisable<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>ible', ':IndentBlanklineEnable<CR>', { noremap = true })
