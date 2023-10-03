6a19c53b68686cb8fa63a8cb7e68c1a711ce92f8
tienshuoc <yorkygrizzly@gmail.com>
2023-10-02 20:04:46 -0700

    added oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  -- Edit filesystem like a normal Neovim buffer.
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+      require('oil').setup()
+  end,
+}
