50bc2ba3c2016084eea4eee8f81f4947a04cd3d2
tienshuoc <yorkygrizzly@gmail.com>
2023-10-02 19:25:36 -0700

    Add buffer delete keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,6 +13,7 @@ vim.keymap.set('n', '<leader>en', ':e ~/.config/nvim/<CR>', opts)  -- Edit neovi
 vim.keymap.set('n', '<leader>w', ':w<CR>', opts)
 vim.keymap.set('n', '<leader>q', ':q<CR>', opts)
 vim.keymap.set('n', '<leader>qa', ':qa<CR>', opts)
+vim.keymap.set('n', '<leader>bd', ':bd<CR>', opts)
 
 vim.keymap.set('n', 'zZ', 'zszH', opts)                                 -- Center cursor on middle of screen horizontal.
 
