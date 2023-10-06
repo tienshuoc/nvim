752a944cfbf85871cd6534b554c4bb5f933dc5c5
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-10-06 13:11:24 -0700

    Add dashboard plugin

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,11 @@
+{
+  -- Dashboard during empty startup.
+  'glepnir/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      -- config
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
