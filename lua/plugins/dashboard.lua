490060b8d72de9bc23cff8d7997c0e96b4081b5a
tienshuoc <yorkygrizzly@gmail.com>
2023-10-08 11:07:04 -0700

    update dashboard settings

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,11 +1,26 @@
 return {
-  -- Dashboard during empty startup.
-  'glepnir/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-      -- config
-    }
-  end,
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+    -- Dashboard during empty startup.
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup {
+            -- config
+            theme = 'hyper', -- 'hyper', 'doom'
+            config = {
+                -- header = {
+                -- "Welcome"
+                -- }
+                shortcut = {
+                    { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                    {
+                        desc = ' NeoVim dotfiles',
+                        group = 'Files',
+                        action = ':e ~/.config/nvim',
+                        key = 'd',
+                    },
+                },
+            },
+        }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
 }
