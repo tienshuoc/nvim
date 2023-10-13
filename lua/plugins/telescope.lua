009645b464738be63329839bd09c4d11d92099c3
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-10-13 13:43:26 -0700

    Don't lazy on telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 --- Telescope (fuzzy finder) ---
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    lazy = false,
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
         { '<leader>ff', '<cmd>Telescope find_files<cr>', mode = 'n', { noremap = true }}, -- Find files.
