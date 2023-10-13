82c63acba6c7a234eca6dd0da551ed9326db9413
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-10-13 16:07:18 -0700

    Update lualine.lua: integrate with coc-nav (winbar information)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,4 +9,32 @@ return {
             }
         }
     end,
+    
+    ------ Integration with `coc-nav` (begin) ------
+    -- `CocInstall coc-nav`
+      local function breadcrumbs()
+          local items = vim.b.coc_nav
+          local t = { '' }
+          for k, v in ipairs(items) do
+              setmetatable(v, {
+                  __index = function(table, key)
+                      return ' '
+                  end
+              })
+              t[#t + 1] = '%#' ..
+                  (v.highlight or "Normal") ..
+                  '#' .. (type(v.label) == 'string' and v.label .. ' ' or '') .. '%#Normal#' .. (v.name or '')
+              if next(items, k) ~= nil then
+                  t[#t + 1] = ' > '
+              end
+          end
+          return table.concat(t)
+      end
+      require('lualine').setup {
+          winbar = {
+              lualine_c = { breadcrumbs }
+          },
+      }
+      ------ Integration with `coc-nav` (end) ------
+
 }
