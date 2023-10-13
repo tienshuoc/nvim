6566145dfbdcf06019fc5999fb4e33279d29b184
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-10-13 13:50:35 -0700

    Update sn_options.lua

--- lua/sn_options.lua
+++ lua/sn_options.lua
@@ -7,3 +7,8 @@ vim.keymap.set('n', '<leader>tl1', ':e $SCRATCH_HOME/1tmp.list<CR>', opts)
 vim.keymap.set('n', '<leader>tl2', ':e $SCRATCH_HOME/2tmp.list<CR>', opts)
 vim.keymap.set('n', '<leader>tl3', ':e $SCRATCH_HOME/3tmp.list<CR>', opts)
 vim.keymap.set('n', '<leader>tl4', ':e $SCRATCH_HOME/4tmp.list<CR>', opts)
+
+vim.keymap.set('n', '<leader>mks1', ':mksession! ~/s1.vim<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>mks2', ':mksession! ~/s2.vim<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>mks3', ':mksession! ~/s3.vim<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>mks4', ':mksession! ~/s4.vim<CR>', { noremap = true })
