5c423f617e731dfad2d7f434c36110adc2718375
tienshuoc <yorkygrizzly@gmail.com>
2023-10-21 19:56:26 -0400

    Yank full path of current file to system keyboard.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,33 +1,35 @@
 -- define common options
 local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
+    noremap = true, -- non-recursive
+    silent = true,  -- do not show message
 }
 
 
 -----------------
 -- Normal mode --
 -----------------
-vim.keymap.set('n', '<leader>en', ':e ~/.config/nvim/<CR>', opts)  -- Edit neovim settings.
+vim.keymap.set('n', '<leader>en', ':e ~/.config/nvim/<CR>', opts) -- Edit neovim settings.
 
 vim.keymap.set('n', '<leader>w', ':w<CR>', opts)
 vim.keymap.set('n', '<leader>q', ':q<CR>', opts)
 vim.keymap.set('n', '<leader>qa', ':qa<CR>', opts)
 vim.keymap.set('n', '<leader>bd', ':bd<CR>', opts)
 
-vim.keymap.set('n', 'zZ', 'zszH', opts)                                 -- Center cursor on middle of screen horizontal.
+vim.keymap.set('n', 'zZ', 'zszH', opts) -- Center cursor on middle of screen horizontal.
 
 vim.keymap.set('n', '<leader>nn', ':set nonumber norelativenumber<CR>:set signcolumn=no<CR>', opts)
 vim.keymap.set('n', '<leader>sn', ':set number relativenumber<CR>:set signcolumn=yes<CR>', opts)
 vim.keymap.set('n', '<leader>san', ':set number norelativenumber<CR>:set signcolumn=yes<CR>', opts)
 
-vim.keymap.set('n', '<leader>sb', ':split<CR><Cmd>BufferPick<CR>', opts)               -- Horizontal split and pick buffer.
-vim.keymap.set('n', '<leader>vsb', ':vertical split<CR><Cmd>BufferPick<CR>', opts)     -- Vertical split and pick buffer.
+vim.keymap.set('n', '<leader>sb', ':split<CR><Cmd>BufferPick<CR>', opts)           -- Horizontal split and pick buffer.
+vim.keymap.set('n', '<leader>vsb', ':vertical split<CR><Cmd>BufferPick<CR>', opts) -- Vertical split and pick buffer.
 
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', opts)            -- Switch panes left.
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', opts)            -- Switch panes down.
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', opts)            -- Switch panes up.
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', opts)            -- Switch panes right.
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', opts)                                -- Switch panes left.
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', opts)                                -- Switch panes down.
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', opts)                                -- Switch panes up.
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', opts)                                -- Switch panes right.
+
+vim.keymap.set('n', '<leader>yrp', ':let @+=expand("%:p")<CR>', opts)              -- Yank current file's full path into system clipboard.
 
 -----------------
 -- Insert mode --
