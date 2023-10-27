f604182d21d4747b0e54856d618ed3b9fdb4e7a0
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-10-27 11:39:10 -0700

    Fix delete not using hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -2,19 +2,28 @@ return {
     'phaazon/hop.nvim',
     branch = 'v2', -- optional but strongly recommended
     keys = {
-
-        { 'f', function() require('hop').hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true }) end, mode = 'n', { remap = true } },
-        { 'F', function() require('hop').hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true }) end, mode = 'n', { remap = true } },
-        { 't', function() require('hop').hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end, mode = 'n', { remap = true } },
-        { 'T', function() require('hop').hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end, mode = 'n', { remap = true } },
-
         { '<leader>hw', ':HopWordMW<CR>', mode = 'n', { noremap = true, silent = true } },
         { '<leader>ha', ':HopAnywhere<CR>', mode = 'n', { noremap = true, silent = true } },
         { '<leader>hp', ':HopPattern<CR>', mode = 'n', { noremap = true, silent = true } },
-
     },
     config = function()
         -- you can configure Hop the way you like here; see :h hop-config
         require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+
+        local hop = require('hop')
+        local directions = require('hop.hint').HintDirection
+        vim.keymap.set('', 'f', function()
+            hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+        end, { remap = true })
+        vim.keymap.set('', 'F', function()
+            hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+        end, { remap = true })
+        vim.keymap.set('', 't', function()
+            hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+        end, { remap = true })
+        vim.keymap.set('', 'T', function()
+            hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+        end, { remap = true })
+
     end
 }
