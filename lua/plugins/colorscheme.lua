1badc194e68faea0f7da9151a9963b2d0e91c3f7
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-11-30 16:22:05 -0800

    Add Everforest colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -130,7 +130,16 @@ return {
             -- vim.cmd(" colorscheme material ")
         end,
     },
-
-
-
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000, -- make sure to load this before all the other start plugins
+        -- Optional; default configuration will be used if setup isn't called.
+        config = function()
+            require("everforest").setup({
+            -- Your config here
+            })
+        end,
+    }
 }
