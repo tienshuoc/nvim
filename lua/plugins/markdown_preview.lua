7ae5e79de8225234fa815a5c9f0e8295b714d9da
tienshuoc <yorkygrizzly@gmail.com>
2023-12-08 19:34:51 -0800

    add markdown preview

---
+++ lua/plugins/markdown_preview.lua
@@ -0,0 +1,10 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_auto_start = 1  -- Open the preview window after entering the Markdown buffer.
+  end,
+  ft = { "markdown" },
+}
