b5cfb6fef5adf428f44b8c3ee5e557f32ae6f096
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-01-18 13:34:19 -0800

    Update markdown_preview.lua: reuse preview window

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -5,6 +5,7 @@ return {
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
     vim.g.mkdp_auto_start = 1  -- Open the preview window after entering the Markdown buffer.
+    vim.g.mkdp_combine_preview = 1  -- Reuse previous opened preview window.
   end,
   ft = { "markdown" },
 }
