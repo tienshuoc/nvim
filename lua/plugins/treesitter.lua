b55e46b592a15aa3621363653c667fad06ea82cb
tienshuoc <yorkygrizzly@gmail.com>
2024-01-18 23:09:47 -0500

    update treesitter files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,23 @@
 return {
 
-  -- Better syntax highlighting
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  dependencies = {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-  },
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        "typescript", "cpp", "jsonc", "markdown", "gitcommit", "bash", "javascript", "python",
-        "lua",
-      },
-      -- sync_install = true,
-      -- auto_install = true,
-      highlight = {
-        enable = true,
-      },
-    })
-  end,
+    -- Better syntax highlighting
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    dependencies = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = {
+                "help", "typescript", "cpp", "jsonc", "markdown",
+                "gitcommit", "bash", "javascript", "python", "lua",
+            },
+            -- sync_install = true,
+            -- auto_install = true,
+            highlight = {
+                enable = true,
+            },
+        })
+    end,
 }
