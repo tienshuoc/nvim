0f2497fbe2d908306e1bc42f29b6019a7345a1d5
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-01-19 07:58:12 -0800

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,6 +13,7 @@ return {
                 "typescript", "cpp", "jsonc", "markdown",
                 "gitcommit", "bash", "javascript", "python", "lua",
             },
+            ignore_install = { "mlir" },  -- Tries to complain that you don't have "treesitter-cli", and installing it gives GLIBC problems. Back off for MLIR.
             sync_install = false,
             auto_install = true,
             highlight = {
