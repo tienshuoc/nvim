1caaefa9112231efaca53f79ba4773f2c7042b37
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-01-19 08:24:57 -0800

    Update bufexplorer.lua: use native commands

--- lua/plugins/bufexplorer.lua
+++ lua/plugins/bufexplorer.lua
@@ -3,7 +3,7 @@ return {
     'jlanzarotta/bufexplorer',
     keys = {
         { '<leader>F', ':BufExplorer<CR>', mode = 'n', { noremap = true, silent = true }},
-        { '<leader>vsF', '<CR>:vs<CR>:BufExplorer<CR>', mode = 'n', { noremap = true, silent = true }},
-        { '<leader>sF', '<CR>:split<CR>:BufExplorer<CR>', mode = 'n', { noremap = true, silent = true }},
+        { '<leader>vsF', vim.cmd.BufExplorerVerticalSplit, mode = 'n', { noremap = true, silent = true }},
+        { '<leader>sF', vim.cmd.BufExplorerHorizontalSplit, mode = 'n', { noremap = true, silent = true }},
     },
 }
