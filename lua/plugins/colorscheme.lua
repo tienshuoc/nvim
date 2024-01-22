584d1ef959939cf12be5b9f5d7bd30f5ea592558
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-01-22 09:44:35 -0800

    Update colorscheme.lua: fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -102,7 +102,7 @@ return {
         'NLKNguyen/papercolor-theme',
         lazy = false,
         config = function()
-           require('PaperColor').setup({
+           require('papercolor-theme').setup({
                     transparent_background = 1,
                 })
           vim.cmd.colorscheme("PaperColor")
