93bbf31216f32d3cac631bcb6fc3e6ed09829d6a
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-02-08 11:46:17 -0700

    Delete lua/plugins/bufexplorer.lua : Relying on Telescope should be sufficient

--- lua/plugins/bufexplorer.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- Buffer Explorer
-    'jlanzarotta/bufexplorer',
-    keys = {
-        { '<leader>F', ':BufExplorer<CR>', mode = 'n', { noremap = true, silent = true }},
-        { '<leader>vsF', vim.cmd.BufExplorerVerticalSplit, mode = 'n', { noremap = true, silent = true }},
-        { '<leader>sF', vim.cmd.BufExplorerHorizontalSplit, mode = 'n', { noremap = true, silent = true }},
-    },
-}
