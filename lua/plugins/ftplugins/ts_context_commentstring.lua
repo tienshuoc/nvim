b2b58d79744ad4ff641c10b5dab00209f16ea74c
tienshuoc <yorkygrizzly@gmail.com>
2024-03-02 22:43:09 -0800

    Move plugin file

---
+++ lua/plugins/ftplugins/ts_context_commentstring.lua
@@ -0,0 +1,8 @@
+return {
+  -- TSX Context Commenting
+  'JoosepAlviste/nvim-ts-context-commentstring',
+  config = function()
+    require('ts_context_commentstring').setup {}
+  end,
+
+}
--- lua/plugins/ts_context_commentstring.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- TSX Context Commenting
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    config = function()
-        require('ts_context_commentstring').setup {}
-    end,
-
-}
