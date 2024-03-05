7b609ab9a9bd25debea2ab0cbeca5e2adf21a96f
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-03-05 11:56:32 -0800

    Update vim_fugitive.lua: change diff keymap

--- lua/plugins/git/vim_fugitive.lua
+++ lua/plugins/git/vim_fugitive.lua
@@ -2,7 +2,7 @@ return {
     'tpope/vim-fugitive',
     lazy = false,
     keys = {
-        { '<leader>dt', ':Git difftool -y<CR>', mode = 'n', { noremap = true, silent = true } },
+        { '<leader>gd', ':Git difftool -y<CR>', mode = 'n', { noremap = true, silent = true } },
         { '<leader>gs', vim.cmd.Git,            mode = 'n', { noremap = true, silent = true } }
     }
 }
