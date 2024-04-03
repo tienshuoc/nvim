71cd93da01c1d66163e1881703a9996d04525155
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-02 22:49:53 -0700

    Update vim_fugitive.lua: add remote url permalink getter.

--- lua/plugins/git/vim_fugitive.lua
+++ lua/plugins/git/vim_fugitive.lua
@@ -1,3 +1,15 @@
+-- Yank a shareable github permalink url for the current file + line to clipboard register.
+vim.keymap.set("n", "<leader>gU", function()
+    local url = ""  -- Add remote repository url here.
+        .. vim.fn["fugitive#RevParse"]("HEAD")
+        .. "/"
+        .. vim.fn.expand("%")
+        .. "#L"
+        .. vim.fn.line(".")
+    vim.fn.setreg("+", url)
+    vim.cmd.echo('"' .. url .. '"')
+end, { desc = "Yank GitHub URL to clipboard (permalink)." })
+
 return {
     'tpope/vim-fugitive',
     lazy = false,
