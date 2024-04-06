58ba3716ea3fa55fb5e48235f8a1070980f13d6d
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-05 22:21:09 -0700

    Update gitsigns.lua
    add git blame short cut

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -1,11 +1,22 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require('gitsigns').setup({
-            current_line_blame = true,
-            current_line_blame_opts = {
-                delay = 0,
-            }
-        })
-    end,
+	"lewis6991/gitsigns.nvim",
+	ops = {
+		signs = {
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "" },
+			topdelete = { text = "" },
+			changedelete = { text = "▎" },
+			untracked = { text = "▎" },
+		},
+	},
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 0,
+			},
+		})
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>")
+	end,
 }
