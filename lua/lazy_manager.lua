aee5a0ceb572ee1660bfe8c74706b6bffcd30610
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-05 22:19:37 -0700

    Update lazy_manager.lua
    add lsp folder

--- lua/lazy_manager.lua
+++ lua/lazy_manager.lua
@@ -1,22 +1,24 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-    { import = 'plugins' },
-    { import = 'plugins.git' },
-    { import = 'plugins.ftplugins' },
-    { import = 'plugins.colorschemes' },
-    -- Debugger plugins.
-    { import = 'plugins.dbg' },
-    { import = 'plugins.dbg.lang' },
+require("lazy").setup({
+	{ import = "plugins" },
+	{ import = "plugins.git" },
+	{ import = "plugins.ftplugins" },
+	{ import = "plugins.colorschemes" },
+	-- Debugger plugins.
+	{ import = "plugins.dbg" },
+	{ import = "plugins.dbg.lang" },
+	-- Lsp plugins.
+	{ import = "plugins.lsp" },
 })
