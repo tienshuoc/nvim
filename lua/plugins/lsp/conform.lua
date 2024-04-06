4fe02e922ab87cddbe1d83c81c3694027f07cd03
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-05 22:23:21 -0700

    Create conform.lua

---
+++ lua/plugins/lsp/conform.lua
@@ -0,0 +1,45 @@
+return {
+	"stevearc/conform.nvim",
+	dependencies = { "mason.nvim" },
+	-- lazy = true,
+	event = { "BufReadPre", "BufNewFile" }, -- Starting to edit / New File.
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>cF",
+			function()
+				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+	},
+	config = function()
+		require("conform").setup({
+			formatters = {
+				clang_format = {
+					command = "clang-format",
+				},
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { { "prettierd", "prettier" } },
+				cpp = { "clang_format" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
+}
