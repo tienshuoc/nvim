72c30799cafab9ac4806afbb06c7b0c540585eb0
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-07 23:35:21 -0700

    Update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,32 +8,5 @@ return {
                 lualine_z = { { 'datetime', style = '%H:%M:%S | %b-%d' } }
             }
         }
-    
-    ------ Integration with `coc-nav` (begin) ------
-    -- `CocInstall coc-nav`
-      local function breadcrumbs()
-          local items = vim.b.coc_nav
-          local t = { '' }
-          for k, v in ipairs(items) do
-              setmetatable(v, {
-                  __index = function(table, key)
-                      return ' '
-                  end
-              })
-              t[#t + 1] = '%#' ..
-                  (v.highlight or "Normal") ..
-                  '#' .. (type(v.label) == 'string' and v.label .. ' ' or '') .. '%#Normal#' .. (v.name or '')
-              if next(items, k) ~= nil then
-                  t[#t + 1] = ' > '
-              end
-          end
-          return table.concat(t)
-      end
-      require('lualine').setup {
-          winbar = {
-              lualine_c = { breadcrumbs }
-          },
-      }
-      ------ Integration with `coc-nav` (end) ------
     end,
 }
