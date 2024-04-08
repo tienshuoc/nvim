0027435c2abb10f1f75aa1cc98e4492c6afc7b29
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-07 23:18:06 -0700

    Update surround.lua

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,4 +2,5 @@ return {
   -- Surround
   "kylechui/nvim-surround",
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  config = true, -- Runs require('nvim-surround').setup().
 }
