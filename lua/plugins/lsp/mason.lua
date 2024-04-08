1aa78947f4d1042d554561629d721295536151c1
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-07 19:01:40 -0700

    Update mason.lua

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,6 +1,47 @@
 -- Package manager for LSP servers, DAP servers, linters, and formatters.
 return {
   "williamboman/mason.nvim",
-  lazy = true, -- Manage loading in `nvim_lspconfig.lua`.
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim", -- Bridges the gap btw `mason.nvim` and `lspconfig`.
+    "WhoIsSethDaniel/mason-tool-installer.nvim", -- Install and upgrade third-party tools automatically.
+  },
   build = ":MasonUpdate",
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- Enable `mason` and configure icons.
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      -- List of servers for mason to install.
+      ensure_installed = {
+        "tsserver",
+        "clangd",
+        "lua_ls",
+        "pyright",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        -- "prettier", -- Prettier formatter.
+        "clang-format",
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
+    })
+  end,
 }
