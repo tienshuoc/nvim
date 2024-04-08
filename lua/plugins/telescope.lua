e9fb7f4e38b94001409c01c506d03f5ed0a01279
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-07 19:41:33 -0700

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,35 +1,36 @@
 --- Telescope (fuzzy finder) ---
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.3',
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.3",
   lazy = false,
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { '<leader>ff', '<cmd>Telescope find_files<cr>',  mode = 'n', { noremap = true } }, -- Find files.
-    { '<leader>fg', '<cmd>Telescope live_grep<cr>',   mode = 'n', { noremap = true } }, -- Grep amongst files.
-    { '<leader>fb', '<cmd>Telescope buffers<cr>',     mode = 'n', { noremap = true } }, -- Find files in buffer list.
-    { '<leader>fh', '<cmd>Telescope help_tags<cr>',   mode = 'n', { noremap = true } }, -- Find help tags.
-    { '<leader>tc', '<cmd>Telescope colorscheme<cr>', mode = 'n', { noremap = true } }, -- Shortcut to browsing colorschemes.
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", mode = "n", { noremap = true } }, -- Find files.
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", mode = "n", { noremap = true } }, -- Grep amongst files.
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", mode = "n", { noremap = true } }, -- Find files in buffer list.
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", mode = "n", { noremap = true } }, -- Find help tags.
+    { "<leader>tc", "<cmd>Telescope colorscheme<cr>", mode = "n", { noremap = true } }, -- Shortcut to browsing colorschemes.
+    { "<leader>ss", "<cmd>Telescope lsp_document_symbols symbols='function'<cr>", mode = "n", { noremap = true } }, -- Search functions in current buffer.
   },
   config = function()
-    local telescope = require('telescope')
+    local telescope = require("telescope")
     telescope.setup({
       pickers = {
         colorscheme = {
-          enable_preview = true, -- Preview colorschemes with `:Telescope colorscheme`
-        }
+          enable_preview = true,
+        },
       },
       defaults = {
         path_display = {
           shorten = {
             len = 5,
-            exclude = { 1, -1 } -- Truncate to show 5 letters, except first and first to last.
-          }
+            exclude = { 1, -1 }, -- Truncate to show 5 letters, except first and first to last.
+          },
         },
         wrap_results = true, -- Enable wrap around.
-        mappings = {         -- Allow Ctrl-U to clear in insert mode.
+        mappings = { -- Allow Ctrl-U to clear in insert mode.
           i = {
-            ["<C-u>"] = false
+            ["<C-u>"] = false,
           },
         },
         file_ignore_patterns = {
@@ -39,6 +40,5 @@ return {
         },
       },
     })
-    -- require('telescope').load_extension('builtin') -- Load the 'builtin' extension
   end,
 }
