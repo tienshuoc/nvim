b7baaaa0ea9578e55c3fb13eaf90532e4a5b476a
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-08 22:20:30 -0700

    Update nvim_lspconfig.lua

--- lua/plugins/lsp/nvim_lspconfig.lua
+++ lua/plugins/lsp/nvim_lspconfig.lua
@@ -35,7 +35,6 @@ return {
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
     vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
         -- Enable completion triggered by <c-x><c-o>
         vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
