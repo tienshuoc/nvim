21f97329017ed9d7d5453efd44f16f078fcff346
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-08 22:19:25 -0700

    Update nvim_lspconfig.lua

--- lua/plugins/lsp/nvim_lspconfig.lua
+++ lua/plugins/lsp/nvim_lspconfig.lua
@@ -31,7 +31,6 @@ return {
     vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
