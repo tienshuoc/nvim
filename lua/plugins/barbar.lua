aa684826ae5fb15a380d7a400fa006a34f918cdd
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-09 22:43:24 -0700

    Update and rename barbar.lua to bufferline.lua
    Replace barbar with bufferline.

--- lua/plugins/barbar.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- Buffer Tabs
-    'romgrk/barbar.nvim',
-    keys = {
-        { '<leader>B', '<Cmd>BufferPick<CR>', mode = 'n', { noremap = true, silent = true } },
-        { '<leader>D', '<Cmd>BufferPickDelete<CR>', mode = 'n', { noremap = true, silent = true } },
-    },
-}
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  -- Buffer Tabs
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  keys = {
+    { "<leader>B", "<Cmd>BufferLinePick<CR>", mode = "n", { noremap = true, silent = true } },
+    { "<leader>D", "<Cmd>BufferPickDelete<CR>", mode = "n", { noremap = true, silent = true } },
+  },
+  config = function()
+    vim.opt.termguicolors = true
+    require("bufferline").setup({})
+  end,
+}
