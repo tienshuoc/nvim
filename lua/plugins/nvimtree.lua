3c583c29cd3c60c8f1859ea4fb0ad3145ac342ce
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-13 11:18:06 -0700

    Update nvimtree.lua

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,21 +1,34 @@
 return {
-    -- Nvim Tree (file explorer)
-    'nvim-tree/nvim-tree.lua',
-    lazy = false,
-    dependencies = {
-        'nvim-tree/nvim-web-devicons', -- Icons.
-    },
-    keys = {
-        { '<leader>nt', ':NvimTreeToggle<CR>', mode = 'n', { noremap = true, silent = true }},
-    },
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        require("nvim-tree").setup({
-            update_focused_file = {
-                enable = true,
-                update_cwd = false,
-            }
-        })
-    end,
+  -- Nvim Tree (file explorer)
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- Icons.
+  },
+  keys = {
+    { "<leader>nt", ":NvimTreeToggle<CR>", mode = "n", { noremap = true, silent = true } },
+  },
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    require("nvim-tree").setup({
+      update_focused_file = {
+        enable = true,
+        update_cwd = false,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+          inline_arrows = true,
+          icons = {
+            corner = "└",
+            edge = "│",
+            item = "│",
+            bottom = "─",
+            none = " ",
+          },
+        },
+      },
+    })
+  end,
 }
