60ada19e5cde452bd63b5415c14ae311c514a414
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-15 16:44:29 -0700

    Update bufferline.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   keys = {
     { "<leader>B", "<Cmd>BufferLinePick<CR>", mode = "n", { noremap = true, silent = true } },
-    { "<leader>D", "<Cmd>BufferLineDelete<CR>", mode = "n", { noremap = true, silent = true } },
+    { "<leader>D", "<Cmd>BufferLinePickClose<CR>", mode = "n", { noremap = true, silent = true } },
   },
   config = function()
     vim.opt.termguicolors = true
