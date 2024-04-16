946eab0d1b46d6d9c6ad7bcc5c7db6e35101d685
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-16 10:43:57 -0700

    Update harpoon.lua: Address deprecation

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,7 +4,7 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local harpoon = require('harpoon')
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)                      -- Add current file to harpoon list.
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)                      -- Add current file to harpoon list.
     vim.keymap.set('n', '<leader>e', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end) -- Toggle harpoon list.
   end,
 }
