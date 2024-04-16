0de9bf25e29c6851827fd31365f8c332c39069fa
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-16 11:08:18 -0700

    Update treesitter_textobjects.lua

--- lua/plugins/treesitter_textobjects.lua
+++ lua/plugins/treesitter_textobjects.lua
@@ -63,7 +63,7 @@ return {
                     goto_next_start = {
                         ["]f"] = { query = "@call.outer", desc = "Next function call start" },
                         ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
-                        ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                        -- ["]c"] = { query = "@class.outer", desc = "Next class start" }, -- This conflicts with "goto changes in git difftool".
                         ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
                         ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
 
@@ -82,7 +82,7 @@ return {
                     goto_previous_start = {
                         ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
                         ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
-                        ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+                        -- ["[c"] = { query = "@class.outer", desc = "Prev class start" }, -- This conflicts with "goto changes in git difftool".
                         ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
                         ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
                     },
