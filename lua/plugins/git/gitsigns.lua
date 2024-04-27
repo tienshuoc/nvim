d75fdd42405e438bbaebb6012b773dbfdf12272a
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-27 16:35:51 -0700

    Update gitsigns.lua

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -1,22 +1,24 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	ops = {
-		signs = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "" },
-			topdelete = { text = "" },
-			changedelete = { text = "▎" },
-			untracked = { text = "▎" },
-		},
-	},
-	config = function()
-		require("gitsigns").setup({
-			current_line_blame = true,
-			current_line_blame_opts = {
-				delay = 0,
-			},
-		})
-		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>")
-	end,
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  ops = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+  },
+  config = function()
+    vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { italic = true, fg = "#414550" })
+    require("gitsigns").setup({
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 0,
+      },
+    })
+    vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>")
+  end,
 }
