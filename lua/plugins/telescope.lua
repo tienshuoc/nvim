4244c19c9b6fb666723001a1868e7c9a5127736f
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-04-30 14:50:12 -0700

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,24 @@
+local function filename_first(_, path)
+  local tail = vim.fs.basename(path)
+  local parent = vim.fs.dirname(path)
+  if parent == "." then
+    return tail
+  end
+  return string.format("%s\t\t%s", tail, parent)
+end
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "TelescopeResults",
+  callback = function(ctx)
+    vim.api.nvim_buf_call(ctx.buf, function()
+      vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+      vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+    end)
+  end,
+})
 --- Telescope (fuzzy finder) ---
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.3",
+  branch = "0.1.x",
   lazy = false,
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
@@ -20,25 +37,17 @@ return {
   config = function()
     local telescope = require("telescope")
     telescope.setup({
-      pickers = {
-        colorscheme = {
-          enable_preview = true,
-        },
-        help_tags = {
-          mappings = {
-            i = {
-              ["<CR>"] = "file_vsplit", -- Do vertical split by default.
-            },
-          },
-        },
-      },
-      defaults = {
-        path_display = {
-          shorten = {
-            len = 5,
-            exclude = { 1, -1 }, -- Truncate to show 5 letters, except first and first to last.
-          },
-        },
+      defaults = { -- Default configuration for telescope.
+        -- path_display = {
+        --   filename_first = {
+        --     reverse_directories = false,
+        --   },
+        --   shorten = {
+        --     len = 5,
+        --     exclude = { 1, -1 }, -- Truncate to show 5 letters, except first and first to last.
+        --   },
+        -- },
+        path_display = filename_first,  -- This is going to be native soon in the Release, using a custom func for now to do this.
         wrap_results = true, -- Enable wrap around.
         mappings = {
           i = {
@@ -49,6 +58,18 @@ return {
           "package%-lock.json",
           "package.json",
           "yarn.lock",
+        },
+      },
+      pickers = { -- Default configuration for builtin pickers.
+        colorscheme = {
+          enable_preview = true,
+        },
+        help_tags = {
+          mappings = {
+            i = {
+              ["<CR>"] = "file_vsplit", -- Do vertical split by default.
+            },
+          },
         },
       },
     })
