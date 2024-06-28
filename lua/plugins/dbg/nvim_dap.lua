25ab4af17d057f83c34c3fb9642a16f9e47467b6
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-06-27 23:01:54 -0700

    Update nvim_dap.lua

--- lua/plugins/dbg/nvim_dap.lua
+++ lua/plugins/dbg/nvim_dap.lua
@@ -12,63 +12,44 @@ return {
     { "<leader>db", ":DapToggleBreakpoint<CR>", desc = "DapToggleBreakpoint" },
   },
   config = function()
-    local dap = require("dap")
-
-    -- Use nvim-dap events to open/close dap-ui windows automatically.
-    local dapui = require("dapui")
-    dapui.setup()
-    vim.keymap.set("n", "<leader>dui", dapui.toggle)
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-
     -- Keymaps for debugger.
     vim.keymap.set("n", "<leader>dC", function()
       dap.clear_breakpoints()
       -- require("notify")("Breakpoints cleared", "warn")
-    end)
+    end, { desc = "Clear breakpoints." })
 
     -- C++ (begin)
-    dap.adapters.lldb = {
-      type = "executable",
-      command = "lldb-vscode", -- Use absolute path if necessary.
-      name = "lldb",
-    }
-    dap.configurations.cpp = {
-      {
-        name = "Launch",
-        type = "lldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
-
-        -- 💀
-        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-        --
-        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-        --
-        -- Otherwise you might get the following error:
-        --
-        --    Error on launch: Failed to attach to the target process
-        --
-        -- But you should be aware of the implications:
-        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-        -- runInTerminal = false,
-      },
-    }
+    -- dap.adapters.lldb = {
+    --   type = "executable",
+    --   command = "/opt/sambanova/llvm16/bin/lldb", -- Use absolute path if necessary.
+    --   name = "lldb",
+    -- }
+    -- dap.configurations.cpp = {
+    --   {
+    --     name = "Launch",
+    --     type = "lldb",
+    --     request = "launch",
+    --     program = function()
+    --       return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    --     end,
+    --     cwd = "${workspaceFolder}",
+    --     stopOnEntry = false,
+    --     args = {},
+    --
+    --     -- 💀
+    --     -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+    --     --
+    --     --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    --     --
+    --     -- Otherwise you might get the following error:
+    --     --
+    --     --    Error on launch: Failed to attach to the target process
+    --     --
+    --     -- But you should be aware of the implications:
+    --     -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+    --     -- runInTerminal = false,
+    --   },
+    -- }
     -- C++ (end)
   end,
 }
