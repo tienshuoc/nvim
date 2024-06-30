9d0fc45d45eba33b85bf9f432146e99758cbb593
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-06-30 13:39:42 -0700

    Update mason_nvim_dap.lua

--- lua/plugins/dbg/mason_nvim_dap.lua
+++ lua/plugins/dbg/mason_nvim_dap.lua
@@ -6,7 +6,55 @@ return {
     "mfussenegger/nvim-dap",
   },
   opts = {
-    handlers = {},
+    handlers = {
+      -- function(config)
+      --   -- all sources with no handler get passed here
+      --
+      --   -- Keep original functionality
+      --   require("mason-nvim-dap").default_setup(config)
+      -- end,
+      -- cpp = function(config)
+      --   config.adapters = {
+      --     type = "executable",
+      --     command = "/opt/sambanova/llvm16/bin/lldb",
+      --     args = function()
+      --       return vim.fn.input("Args to executable: ")
+      --     end,
+      --   }
+      --   require("mason-nvim-dap").default_setup(config)
+      -- end,
+    },
+    ensure_installed = {
+      "codelldb",
+    },
+  },
+}
+return {
+  "jay-babu/mason-nvim-dap.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "mfussenegger/nvim-dap",
+  },
+  opts = {
+    handlers = {
+      -- function(config)
+      --   -- all sources with no handler get passed here
+      --
+      --   -- Keep original functionality
+      --   require("mason-nvim-dap").default_setup(config)
+      -- end,
+      -- cpp = function(config)
+      --   config.adapters = {
+      --     type = "executable",
+      --     command = "/opt/sambanova/llvm16/bin/lldb",
+      --     args = function()
+      --       return vim.fn.input("Args to executable: ")
+      --     end,
+      --   }
+      --   require("mason-nvim-dap").default_setup(config)
+      -- end,
+    },
     ensure_installed = {
       "codelldb",
     },
