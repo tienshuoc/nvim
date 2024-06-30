6c8d83803fad61d94dc04c8ab1f84544837acfdc
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-06-30 14:52:48 -0700

    Update manage_colorscheme.lua: set default

--- lua/manage_colorscheme.lua
+++ lua/manage_colorscheme.lua
@@ -3,20 +3,25 @@
 local colorscheme_fname = vim.fn.expand(vim.fn.stdpath("config") .. "/colorscheme_value")
 
 -- Read colorscheme from file.
-vim.cmd.colorscheme(vim.fn.readfile(colorscheme_fname)[1])
+local cmd, err = vim.ui.open(colorscheme_fname)
+if cmd == nil then
+  vim.cmd.colorscheme("vscode")
+else
+  vim.cmd.colorscheme(vim.fn.readfile(colorscheme_fname)[1])
+end
 
 -- Define a function to write the current color scheme to a file
 function write_color_scheme()
-    local colorscheme = vim.g.colors_name
-    -- local colorscheme_file = vim.fn.stdpath('config') .. '/last_colorscheme.vim'
-    -- local cmd = string.format('echo "let g:colors_name = \'%s\'" > %s', colorscheme, colorscheme_file)
-    -- vim.fn.system(cmd)
-    vim.fn.writefile({ colorscheme }, colorscheme_fname)
+  local colorscheme = vim.g.colors_name
+  -- local colorscheme_file = vim.fn.stdpath('config') .. '/last_colorscheme.vim'
+  -- local cmd = string.format('echo "let g:colors_name = \'%s\'" > %s', colorscheme, colorscheme_file)
+  -- vim.fn.system(cmd)
+  vim.fn.writefile({ colorscheme }, colorscheme_fname)
 end
 
 -- Automatically execute the function when the color scheme changes
 vim.api.nvim_create_autocmd("ColorScheme", {
-    callback = function()
-        write_color_scheme()
-    end,
+  callback = function()
+    write_color_scheme()
+  end,
 })
