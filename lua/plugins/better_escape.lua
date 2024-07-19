d5fe47fe6a253bea1deab705f20be73ef2f659d2
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-07-19 15:19:38 -0700

    Update better_escape.lua

--- lua/plugins/better_escape.lua
+++ lua/plugins/better_escape.lua
@@ -2,11 +2,19 @@ return {
   -- Escape insert mode quickly with customized key combination without lag.
   "max397574/better-escape.nvim",
   config = function()
-    require("better_escape").setup {
-      mapping = { "yy", "jj" },        -- a table with mappings to use
-      timeout = 200,                   -- the time in which the keys must be hit in ms. Use `vim.o.timeoutlen` by default
-      clear_empty_lines = false,       -- clear line after escaping if there is only whitespace
-      keys = "<Esc>",                  -- keys used for escaping, if it is a function will use the result everytime
-    }
+    require("better_escape").setup({
+      timeout = 200, -- the time in which the keys must be hit in ms. Use `vim.o.timeoutlen` by default
+      default_mappings = false,
+      mappings = {
+        i = {
+          j = {
+            j = "<Esc>",
+          },
+          y = {
+            y = "<Esc>",
+          },
+        },
+      },
+    })
   end,
 }
