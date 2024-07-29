7dfff729bd4d5cc225964b823cdec853ff278b59
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-07-29 07:21:00 -0700

    Update cyberdream.lua

--- lua/plugins/colorschemes/cyberdream.lua
+++ lua/plugins/colorschemes/cyberdream.lua
@@ -1,5 +1,12 @@
 return {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  priority = 1000,
+
+  config = function()
+    require("cyberdream").setup({
+      transparent = true,
+      italic_comments = true,
+    })
+  end,
 }
