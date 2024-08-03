513c0cd1ee9d061accf01e6b6aa4275aabdf8539
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-08-03 15:51:45 -0700

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,30 +9,44 @@ local opts = {
 ----------------------------------------------------------------------------------------------------------------------------------------
 vim.keymap.set("n", "<leader>nv", ":e ~/.config/nvim/<CR>", opts) -- Edit neovim settings.
 
+opts["desc"] = "Write file."
 vim.keymap.set("n", "<leader>w", ":w<CR>", opts)
+opts["desc"] = "Quit file."
 vim.keymap.set("n", "<leader>qq", ":q<CR>", opts)
+opts["desc"] = "Quit all."
 vim.keymap.set("n", "<leader>qa", ":qa<CR>", opts)
+opts["desc"] = "Close buffer."
 vim.keymap.set("n", "<leader>bd", ":bd<CR>", opts)
+opts["desc"] = "Close tab."
 vim.keymap.set("n", "<leader>tc", ":tabc<CR>", opts)
+opts["desc"] = "Switch to previous buffer."
 vim.keymap.set("n", "<leader>tt", "<C-^>", opts) -- Switch to previous buffer.
 
-vim.keymap.set("n", "zZ", "zszH", opts) -- Center cursor on middle of screen horizontal.
-vim.keymap.set("n", "n", "nzzzv", opts) -- Keeps next search term in middle of screen.
-vim.keymap.set("n", "N", "Nzzzv", opts) -- Keeps previous search term in middle of screen.
+opts["desc"] = "Center cursor on middle of screen horizontal."
+vim.keymap.set("n", "zZ", "zszH", opts)
+opts["desc"] = "Keeps next search term in middle of screen."
+vim.keymap.set("n", "n", "nzzzv", opts)
+opts["desc"] = "Keeps previous search term in middle of screen."
+vim.keymap.set("n", "N", "Nzzzv", opts)
 
-vim.keymap.set("n", "<leader>nn", ":set nonumber norelativenumber<CR>:set signcolumn=no<CR>", opts)
-vim.keymap.set("n", "<leader>sn", ":set number relativenumber<CR>:set signcolumn=yes<CR>", opts)
-vim.keymap.set("n", "<leader>san", ":set number norelativenumber<CR>:set signcolumn=yes<CR>", opts)
+opts["desc"] = "Toggle relative number."
+vim.keymap.set("n", "<leader>trn", ":set relativenumber!<CR>:set signcolumn=yes<CR>", opts)
+
+opts["desc"] = "Toggle line wrap."
+vim.keymap.set("n", "<leader>tw", ":set wrap!<CR>", opts)
 
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", opts) -- Switch panes left.
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", opts) -- Switch panes down.
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", opts) -- Switch panes up.
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", opts) -- Switch panes right.
 
-vim.keymap.set("n", "<leader>rp", "1<C-G>", opts) -- Show file fullpath.
-vim.keymap.set("n", "<leader>yrp", ':let @+=expand("%:p")<CR>', opts) -- Yank current file's full path into system clipboard.
+opts["desc"] = "Show file fullpath."
+vim.keymap.set("n", "<leader>rp", "1<C-G>", opts)
+opts["desc"] = "Yank current file's full path into system clipboard."
+vim.keymap.set("n", "<leader>yrp", ':let @+=expand("%:p")<CR>', opts)
 
-vim.keymap.set("n", "<leader>ccl", ":ccl<CR>", opts) -- Close quickfix list.
+opts["desc"] = "Close quickfix list."
+vim.keymap.set("n", "<leader>ccl", ":ccl<CR>", opts)
 
 -- Go to window by index.
 vim.keymap.set("n", "<leader>1", "1<C-W>w", { noremap = true, desc = "Move to window 1." })
@@ -56,7 +70,8 @@ vim.keymap.set("i", "yy", "<Esc>", opts)
 vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", opts)
 vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", opts)
 vim.keymap.set("x", "/", "<Esc>/\\%V", opts) -- Search in visual range.
-vim.keymap.set("x", "<leader>p", '"_dP') -- Paste without yanking replaced text into register.
+opts["desc"] = "Paste without yanking replaced text into register."
+vim.keymap.set("x", "<leader>p", '"_dP')
 
 ----------------------------------------------------------------------------------------------------------------------------------------
 -- ========================================================= Terminal mode ===============================================================
