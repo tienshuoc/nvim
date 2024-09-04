ca43d77ee6c160d32bfb09b8a1ce2a03cee6562d
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-09-04 15:39:11 -0700

    Update vim_fugitive.lua

--- lua/plugins/git/vim_fugitive.lua
+++ lua/plugins/git/vim_fugitive.lua
@@ -1,20 +1,20 @@
 -- Yank a shareable github permalink url for the current file + line to clipboard register.
 vim.keymap.set("n", "<leader>gU", function()
-    local url = ""  -- Add remote repository url here.
-        .. vim.fn["fugitive#RevParse"]("HEAD")
-        .. "/"
-        .. vim.fn.expand("%")
-        .. "#L"
-        .. vim.fn.line(".")
-    vim.fn.setreg("+", url)
-    vim.cmd.echo('"' .. url .. '"')
+  local url = "https://github.sambanovasystems.com/SambaNova/software/blob/" -- Add remote repository url here.
+    .. vim.fn["fugitive#RevParse"]("HEAD")
+    .. "/"
+    .. vim.fn.expand("%")
+    .. "#L"
+    .. vim.fn.line(".")
+  vim.fn.setreg("+", url)
+  vim.cmd.echo('"' .. url .. '"')
 end, { desc = "Yank GitHub URL to clipboard (permalink)." })
 
 return {
-    'tpope/vim-fugitive',
-    lazy = false,
-    keys = {
-        { '<leader>gg', ':Git difftool -y<CR>', mode = 'n', { noremap = true, silent = true } },
-        { '<leader>gs', vim.cmd.Git,            mode = 'n', { noremap = true, silent = true } }
-    }
+  "tpope/vim-fugitive",
+  lazy = false,
+  keys = {
+    { "<leader>gg", ":Git difftool -y<CR>", mode = "n", { noremap = true, silent = true } },
+    { "<leader>gs", vim.cmd.Git, mode = "n", { noremap = true, silent = true } },
+  },
 }
