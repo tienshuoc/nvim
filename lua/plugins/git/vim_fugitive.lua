c7277a2b1694f3d0882153b8281925573b7c7565
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-09-04 15:38:04 -0700

    Update vim_fugitive.lua

--- lua/plugins/git/vim_fugitive.lua
+++ lua/plugins/git/vim_fugitive.lua
@@ -14,7 +14,7 @@ return {
     'tpope/vim-fugitive',
     lazy = false,
     keys = {
-        { '<leader>gd', ':Git difftool -y<CR>', mode = 'n', { noremap = true, silent = true } },
+        { '<leader>gg', ':Git difftool -y<CR>', mode = 'n', { noremap = true, silent = true } },
         { '<leader>gs', vim.cmd.Git,            mode = 'n', { noremap = true, silent = true } }
     }
 }
