f97499565e79ced5ccdcb3104b22325461c42f8e
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-09-30 13:26:13 -0700

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.api.nvim_buf_call(ctx.buf, function()
       vim.fn.matchadd("TelescopeParent", "\t\t.*$")
       vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+      vim.api.nvim_set_hl(0, "TelescopeSelection", { link = "CursorLine" })
     end)
   end,
 })
