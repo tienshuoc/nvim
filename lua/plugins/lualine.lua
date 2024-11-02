79573f8404bf49fe08c8e6b2d4e80a5a0dd0e5b6
tienshuoc <yorkygrizzly@gmail.com>
2024-11-02 15:03:41 -0400

    update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ end
 return {
   -- Lualine
   "nvim-lualine/lualine.nvim",
-  dependencies = { { "nvim-tree/nvim-web-devicons", opt = true }, "lsp-progress.nvim" },
+  dependencies = { { "nvim-tree/nvim-web-devicons", opt = true } },
   config = function()
     require("lualine").setup({
       sections = {
