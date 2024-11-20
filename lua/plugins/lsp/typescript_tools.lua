8346790bebbafa215f693d01d7b969d72db300ba
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-11-20 11:41:09 -0800

    Update typescript_tools.lua

--- lua/plugins/lsp/typescript_tools.lua
+++ lua/plugins/lsp/typescript_tools.lua
@@ -2,4 +2,5 @@ return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   opts = {},
+  ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" }, -- Filetypes for lazy-loading
 }
