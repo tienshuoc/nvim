1ec48074353dbbccbe18387af6ad0b8a4d2c2c79
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-11-20 11:36:02 -0800

    Update nvim_dap_virtual_text.lua

--- lua/plugins/lsp/nvim_dap_virtual_text.lua
+++ lua/plugins/lsp/nvim_dap_virtual_text.lua
@@ -1,6 +1,7 @@
 return {
   -- Adds virtual text support to nvim-dap. Displays variables' values beside their definitions.
   "theHamsta/nvim-dap-virtual-text",
+  lazy = true,
   config = function()
     require("nvim-dap-virtual-text").setup()
   end,
