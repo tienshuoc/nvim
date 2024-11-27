0f5e9e3ed02e994e802edcb04f263e8f732973de
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-11-27 15:45:40 -0800

    Update diffview.lua

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,6 +1,6 @@
 return {
   "sindrets/diffview.nvim", -- Git diff page.
-  Lazy = true,  -- Requires Git >= 2.31.0 to work properly.
+  lazy = true,  -- Requires Git >= 2.31.0 to work properly.
   keys = {
     { "<leader>gg", ":DiffviewOpen<CR>", mode = "n", { noremap = true, silent = true } }
   }
