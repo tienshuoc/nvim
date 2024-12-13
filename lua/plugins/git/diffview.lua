336490d276734db4042e1f844b5da716b15f0517
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-12-12 18:18:16 -0800

    Update diffview.lua: don't make lazy

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,6 +1,6 @@
 return {
   "sindrets/diffview.nvim", -- Git diff page.
-  lazy = true, -- Requires Git >= 2.31.0 to work properly.
+  lazy = false, -- Requires Git >= 2.31.0 to work properly.
   keys = {
     { "<leader>gg", ":DiffviewOpen<CR>", mode = "n", { noremap = true, silent = true } },
   },
