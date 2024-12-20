e871c66e1942d61d22b7f6310735cd5fd5626eff
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2024-12-19 20:27:48 -0800

    Update nvim_dap_vscode_js.lua

--- lua/plugins/dbg/lang/nvim_dap_vscode_js.lua
+++ lua/plugins/dbg/lang/nvim_dap_vscode_js.lua
@@ -1,67 +1,43 @@
 return {
---     -- Great tutorial: https://miguelcrespo.co/posts/debugging-javascript-applications-with-neovim/
---     -- nvim-dap adapter for vscode-js-debug.
---     'mxsdev/nvim-dap-vscode-js',
---     dependencies = {
---         "microsoft/vscode-js-debug",
---         -- After install, build it and rename the `dist` directory to `out`.
---         build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
---
---         -- (Alternatively) Manual download: https://github.com/mxsdev/nvim-dap-vscode-js?tab=readme-ov-file#manually
---         -- Move file to `~/.local/share/nvim/lazy/vscode-js-debug`
---     },
---     config = function()
---         require("dap-vscode-js").setup({
---             -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
---             -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug",                         -- Path to vscode-js-debug installation.
---             debugger_path = vim.fn.stdpath('data') .. "/lazy/vscode-js-debug",
---             -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
---             adapters = {
---                 'pwa-node',
---                 'pwa-chrome',
---                 'pwa-msedge',
---                 'node-terminal',
---                 'pwa-extensionHost'
---             }, -- which adapters to register in nvim-dap
---             -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
---             -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
---             -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
---         })
---
---         for _, language in ipairs({
---             "typescript",
---             "javascript",
---             "typescriptreact",
---             "javascriptreact",
---         }) do
---             require("dap").configurations[language] = {
---                 {
---                     -- Debug single `Node.js` files.
---                     type = "pwa-node",
---                     request = "launch",
---                     name = "Launch file",
---                     program = "${file}",
---                     cwd = "${workspaceFolder}",
---                 },
---                 {
---                     -- Debug node processes like `express` applications.
---                     type = "pwa-node",
---                     request = "attach",
---                     name = "Attach",
---                     processId = require 'dap.utils'.pick_process,
---                     cwd = "${workspaceFolder}",
---                 },
---                 {
---                     -- Debug web applications.
---                     -- The line userDataDir will save Chrome profile in a file
---                     type = "pwa-chrome",
---                     request = "launch",
---                     name = "Start Chrome with \"localhost\"",
---                     url = "http://localhost:3000",
---                     webRoot = "${workspaceFolder}",
---                     userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
---                 }
---             }
---         end
---     end,
+  "mxsdev/nvim-dap-vscode-js",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    {
+      "microsoft/vscode-js-debug",
+      build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out", -- Required commands to run after installation and updates.
+    },
+  },
+  -- build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+  config = function()
+    require("dap-vscode-js").setup({
+      node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+      debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug", -- The dap `vscode-js-debug` specified in dependencies above would be installed by lazy, need to point to it for this to work.
+      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+      log_console_level = vim.log.levels.ERROR, -- Logging level for output to console. Set to false to disable console output.
+    })
+
+    for _, language in ipairs({ "typescript", "javascript" }) do
+      require("dap").configurations[language] = {
+        {
+          -- For typescript, make sure you have `sourceMap`: true enbled in tsconfig.json.
+          -- This generates `.map` files to map js code to ts code. This allows debugging ts code when executing js.
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          -- program = "${file}",
+          outFiles = { "${workspaceFolder}/**/dist/*.js" },
+          stopOnEntry = true,
+          program = "dist/day2.js",
+          cwd = "${workspaceFolder}",
+        },
+        -- {
+        --   type = "pwa-node",
+        --   request = "attach",
+        --   name = "Attach",
+        --   processId = require("dap.utils").pick_process,
+        --   cwd = "${workspaceFolder}",
+        -- },
+      }
+    end
+  end,
 }
