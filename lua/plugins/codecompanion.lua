a7f98647fb0f222c2bd0b950182c91e16604f5fd
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-02-17 10:29:00 -0800

    Update codecompanion.lua

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -8,13 +8,32 @@ return {
     require("codecompanion").setup({
       strategies = {
         chat = {
-          adapter = "ollama",
+          adapter = "sambacloud",
+          slash_commands = {
+            ["file"] = {
+              -- Location to the slash command in CodeCompanion
+              callback = "strategies.chat.slash_commands.file",
+              description = "Select a file using Fzf-Lua",
+              opts = {
+                provider = "fzf_lua", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+                contains_code = true,
+              },
+            },
+            ["buffer"] = {
+              callback = "strategies.chat.slash_commands.buffer",
+              description = "Select a buffer using Fzf-Lua",
+              opts = {
+                provider = "fzf_lua",
+                contains_code = true,
+              },
+            },
+          },
         },
         inline = {
-          adapter = "ollama",
+          adapter = "sambacloud",
         },
         agent = {
-          adapter = "ollama",
+          adapter = "sambacloud",
         },
       },
       adapters = {
@@ -27,7 +46,48 @@ return {
             },
           })
         end,
+        sambacloud = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://api.sambanova.ai",
+              api_key = "eb466201-fa25-44e9-8222-d00b2985313b",
+              chat_url = "/v1/chat/completions", -- This is already the default. Just being explicit.
+            },
+            schema = {
+              model = {
+                default = "Meta-Llama-3.1-8B-Instruct",
+              },
+            },
+          })
+        end,
+      },
+      opts = {
+        language = "English", -- This comes with the default system prompt.
+        --   system_prompt = function(opts) -- Override default system prompt.
+        --     return "My new system prompt"
+        --   end,
+      },
+      display = {
+        action_palette = {
+          width = 95,
+          height = 10,
+          prompt = "Prompt ", -- Prompt used for interactive LLM calls
+          provider = "telescope", -- default|telescope|mini_pick
+          opts = {
+            show_default_actions = true, -- Show the default actions in the action palette?
+            show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+          },
+        },
       },
     })
+    vim.keymap.set({ "n", "v" }, "<leader>cct", function()
+      require("codecompanion").toggle()
+    end, { noremap = true, desc = "Toggle CodeCompanionChat" })
+    vim.keymap.set(
+      { "n", "v" },
+      "<leader>ccb",
+      ":CodeCompanion /buffer ",
+      { noremap = true, desc = "CodeCompanion /buffer" } -- "ga": Accept change, "gr": Reject change.
+    )
   end,
 }
