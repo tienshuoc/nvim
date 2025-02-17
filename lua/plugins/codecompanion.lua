138fc6d9647808ad35e29d894449fcac195d5376
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-02-16 20:00:27 -0800

    Create codecompanion.lua

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,33 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "ollama",
+        },
+        inline = {
+          adapter = "ollama",
+        },
+        agent = {
+          adapter = "ollama",
+        },
+      },
+      adapters = {
+        ollama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            schema = {
+              model = {
+                default = "llama3.2",
+              },
+            },
+          })
+        end,
+      },
+    })
+  end,
+}
