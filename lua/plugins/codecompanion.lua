a9b1b0f16b66567dd32b0338aea80a6c6a9aa8b9
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-02-17 15:29:04 -0800

    Update codecompanion.lua

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -47,10 +47,16 @@ return {
           })
         end,
         sambacloud = function()
+          -- Remember to add "$XDG_CONFIG_HOME/shh_my_api_keys.lua" with :
+          -- return {
+          --           sambacloud_api_key = "some_api_key",
+          --       }
+          local ok, api_keys = pcall(require, "shh_my_api_keys")
+          local api_key = ok and api_keys.sambacloud_api_key or nil
           return require("codecompanion.adapters").extend("openai_compatible", {
             env = {
               url = "https://api.sambanova.ai",
-              api_key = "MY_API_KEY",
+              api_key = api_key,
               chat_url = "/v1/chat/completions", -- This is already the default. Just being explicit.
             },
             schema = {
