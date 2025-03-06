963049949e26cf7335be74ad0305f97a5fd1b861
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-03-06 08:30:04 -0800

    Update nvim_cmp.lua

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -1,19 +1,16 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp", -- Source for Neovim's built-in language server client.
     "hrsh7th/cmp-buffer", -- Source for text in buffer.
     "hrsh7th/cmp-path", -- Source for file system paths.
-    {
-      "tzachar/cmp-tabnine", -- Source for tabnine.
-      build = "./install.sh",
-    },
     "hrsh7th/cmp-nvim-lsp-signature-help", -- Source for displaying function signatures with the current parameter emphasized.
-    "rafamadriz/friendly-snippets", -- Snippets collection for a set of different programming languages.
     {
       "L3MON4D3/LuaSnip", -- Snippet engine for Neovim.
-      dependencies = { "rafamadriz/friendly-snippets" },
+      dependencies = {
+        "rafamadriz/friendly-snippets", -- Snippets collection for a set of different programming languages.
+      },
       -- follow latest release.
       version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
       -- install jsregexp (optional!).
@@ -23,6 +20,10 @@ return {
     "onsails/lspkind.nvim", -- VSCode-like pictograms.
     "windwp/nvim-autopairs", -- For completion to include brackets.
     "alexander-born/cmp-bazel", -- bazel target completion and package files
+    {
+      "tzachar/cmp-tabnine", -- Source for tabnine.
+      build = "./install.sh",
+    },
   },
   config = function()
     local cmp = require("cmp")
@@ -48,11 +49,11 @@ return {
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
     local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
 
     -- Loads vscode style snippets from installed plugins (e.g. friendly-snippets).
     require("luasnip.loaders.from_vscode").lazy_load()
 
-    local lspkind = require("lspkind")
     cmp.setup({
       preselect = cmp.PreselectMode.None,
       completion = {
@@ -63,19 +64,33 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       mapping = cmp.mapping.preset.insert({
         ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
         ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
         -- C-b (back) C-f (forward) for snippet placeholder navigation.
         ["<C-s>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace, -- Replaces adjacent text w/ selected item.
-        }),
+        ["<CR>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({
+                select = true,
+              })
+            end
+          else
+            fallback()
+          end
+        end),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
           else
             fallback()
           end
@@ -83,7 +98,7 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
+          elseif luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
