742247d7a763567780a5785dd674642eca67a701
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-03-09 17:29:46 -0700

    Update nvim_dap_ui.lua

--- lua/plugins/dbg/nvim_dap_ui.lua
+++ lua/plugins/dbg/nvim_dap_ui.lua
@@ -8,7 +8,7 @@ return {
     -- Use nvim-dap events to open/close dap-ui windows automatically.
     local dapui = require("dapui")
     dapui.setup()
-    vim.keymap.set("n", "<leader>dui", dapui.toggle, { desc = "Toggle DapUI" })
+    vim.keymap.set("n", "<leader>dui", dapui.toggle, { desc = "Toggle DAP UI" })
     dap.listeners.after.event_initialized["dapui_config"] = function()
       dapui.open()
     end
@@ -18,9 +18,5 @@ return {
     dap.listeners.before.event_exited.dapui_config = function()
       dapui.close()
     end
-
-    vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
-    vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
-    vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
   end,
 }
