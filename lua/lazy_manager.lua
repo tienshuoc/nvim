1b967254ee5e6e0021aea49064ebf169e348299c
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-03-13 18:32:36 -0700

    Update lazy_manager.lua: plugins for vscode

--- lua/lazy_manager.lua
+++ lua/lazy_manager.lua
@@ -1,32 +1,42 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  { import = "plugins" },
-  { import = "plugins.git" },
-  { import = "plugins.ftplugins" },
-  { import = "plugins.colorschemes" },
-  -- Debugger plugins.
-  { import = "plugins.dbg" },
-  { import = "plugins.dbg.lang" },
-  -- Lsp plugins.
-  { import = "plugins.lsp" },
-}, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
-})
+if vim.g.vscode then
+  require("lazy").setup({
+    { import = "plugins.flash" },
+  })
+else
+  require("lazy").setup({
+    { import = "plugins" },
+    { import = "plugins.git" },
+    { import = "plugins.ftplugins" },
+    { import = "plugins.colorschemes" },
+    -- Debugger plugins.
+    { import = "plugins.dbg" },
+    { import = "plugins.dbg.lang" },
+    -- Lsp plugins.
+    { import = "plugins.lsp" },
+  }, {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = {
+      notify = false,
+    },
+  })
+end
