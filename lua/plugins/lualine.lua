74eb8e1b658fce3fb6461dff60cc0c4cab091d89
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-04-06 11:43:05 -0700

    Update lualine.lua: Add lsp status

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -31,7 +31,24 @@ return {
           },
         },
         lualine_c = { "diff" },
-        lualine_x = { "branch", "filetype", "encoding" },
+        lualine_x = {
+          {
+            "lsp_status",
+            icon = "", -- f013
+            symbols = {
+              -- Standard unicode symbols to cycle through for LSP progress:
+              spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+              -- Standard unicode symbol for when LSP is done:
+              done = "✓",
+              -- Delimiter inserted between LSP names:
+              separator = " ",
+            },
+            -- List of LSP names to ignore (e.g., `null-ls`):
+            ignore_lsp = {},
+          },
+          "filetype",
+          "encoding",
+        },
         lualine_y = { "progress" },
         lualine_z = { { "datetime", style = "%H:%M:%S | %b-%d" } },
       },
