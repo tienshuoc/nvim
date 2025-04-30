ca42e83b5882b90bf8cac0cf3e30534d09238096
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-04-29 18:29:59 -0700

    Update overseer.lua: make lazy

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -2,6 +2,7 @@ return {
   -- A task runner and job management plugin.
   -- E.g. Create a custom task that builds a C++ file.
   "stevearc/overseer.nvim",
+  lazy = true,
   opts = {},
   config = function()
     require("overseer").setup()
