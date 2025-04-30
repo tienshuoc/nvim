9c1fe083ad10cf591e1d132adb0243600ee58bc3
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-04-29 18:34:44 -0700

    Update conform.lua: make lazy

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -1,9 +1,10 @@
 return {
   "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  -- lazy = true,
-  event = { "BufReadPre", "BufNewFile" }, -- Starting to edit / New File.
-  cmd = "ConformInfo",
+  event = { "BufWritePre" }, -- When saving file s/t can trigger format on save.
+  cmd = { "ConformInfo" },
+  dependencies = {
+    "williamboman/mason.nvim",
+  },
   keys = {
     {
       "<leader>F",
@@ -18,41 +19,33 @@ return {
       desc = "Format current buffer.",
     },
   },
-  config = function()
-    require("conform").setup({
-      formatters = {
-        -- Self-defined formatters.
-        jsonnet_indent4 = {
-          command = "jsonnetfmt",
-          args = {
-            "--indent",
-            "4",
-            "-",
-          },
+  opts = {
+    formatters = {
+      -- Self-defined formatters.
+      jsonnet_indent4 = {
+        command = "jsonnetfmt",
+        args = {
+          "--indent",
+          "4",
+          "-",
         },
       },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { "prettierd", "prettier" },
-        cpp = { "clang-format" },
-        jsonnet = { "jsonnet_indent4" },
-        sh = { "shfmt" },
-      },
-    })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args)
-        if vim.bo.filetype ~= "lua" and vim.bo.filetype ~= "jsonnet" then
-          -- Don't format any file on save other than lua, jsonnet.
-          return
-        end
-        require("conform").format({
-          bufnr = args.buf,
-        })
-      end,
-    })
-  end,
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { "prettierd", "prettier" },
+      cpp = { "clang-format" },
+      jsonnet = { "jsonnet_indent4" },
+      sh = { "shfmt" },
+    },
+    format_on_save = function(bufnr)
+      if vim.bo.filetype ~= "lua" and vim.bo.filetype ~= "jsonnet" then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
+  },
 }
