9ec23b0429352e442b6fcbace39574c3ec8f4972
Tien-Shuo Chang <tienshuo@berkeley.edu>
2025-05-01 20:19:19 +0000

    remove tagbar

--- lua/plugins/tagbar.lua
+++
@@ -1,8 +0,0 @@
-return {
-    --Tagbar
-    'preservim/tagbar',
-    config = function()
-        vim.keymap.set('n', '<leader>tb', ':TagbarToggle<CR>', { noremap = true, silent = true })
-        -- require('tagbar').setup({})
-    end,
-}
