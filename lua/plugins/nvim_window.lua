a152d1f2e7c4554072ad9471182ab3a607e8bb97
Tien-Shuo Chang <tienshuo@berkeley.edu>
2025-05-28 17:42:55 +0000

    add nvim_window: hopping between panes

---
+++ lua/plugins/nvim_window.lua
@@ -0,0 +1,7 @@
+return {
+  "yorickpeterse/nvim-window",
+  keys = {
+    { "<leader>nw", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
+  },
+  config = true,
+}
