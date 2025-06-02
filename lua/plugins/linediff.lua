46d6864b0985c60a9a350345be9fb27721bec1a4
Tien-Shuo Chang <tienshuo@berkeley.edu>
2025-06-02 22:37:16 +0000

    add linediff keymap

--- lua/plugins/linediff.lua
+++ lua/plugins/linediff.lua
@@ -1,3 +1,11 @@
 return {
   "andrewradev/linediff.vim",
+  keys = {
+    {
+      "<leader>ld",
+      "<Cmd>Linediff<CR>",
+      mode = "n",
+      { noremap = true, silent = true, desc = "Linediff" },
+    },
+  },
 }
