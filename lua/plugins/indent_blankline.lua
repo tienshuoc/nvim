1e1c81cb7887509552cb5d9898d2d88e8ab07490
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2025-06-26 22:47:29 +0000

    fix

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -2,10 +2,27 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   cond = not vim.g.is_large_file_on_startup,
+  scope = { enabled = true },
+  exclude = {
+    filetypes = {
+      "help",
+      "alpha",
+      "dashboard",
+      "neo-tree",
+      "Trouble",
+      "trouble",
+      "lazy",
+      "mason",
+      "notify",
+      "toggleterm",
+      "lazyterm",
+    },
+  },
   opts = {
     indent = {
       char = "│",
       tab_char = "│",
     },
-    main = "ibl",
+  },
+  main = "ibl",
 }
