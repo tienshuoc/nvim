9e10fb086b4f70ffd0bd2749b1438a2ffb3ccf70
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2025-09-13 19:40:22 +0000

    1. Disable gitsigns for large files 2. Add keymap for jumping between hunks 3. Add keymap for preview and staging hunks

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -1,5 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
+  cond = not vim.g.is_large_file_on_startup,
   event = { "BufReadPre", "BufNewFile" },
   ops = {
     signs = {
@@ -19,6 +20,15 @@ return {
         delay = 0,
       },
     })
-    vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>")
+    ---- Keymaps
+    vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { desc = "Gitsigns blame line." })
+
+    -- Navigating hunks.
+    vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<CR>", { desc = "Gitsigns prev hunk." })
+    vim.keymap.set("n", "]h", ":Gitsigns next_hunk<CR>", { desc = "Gitsigns next hunk." })
+
+    -- Staging hunks.
+    vim.keymap.set("n", "<leader>gph", ":Gitsigns preview_hunk<CR>", { desc = "Gitsigns preview hunk." })
+    vim.keymap.set("n", "<leader>gsh", ":Gitsigns stage_hunk<CR>", { desc = "Gitsigns stage hunk." })
   end,
 }
