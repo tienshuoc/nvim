69eedabd61ceb20cae483a4cc927d592f3b2c74e
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2025-09-13 19:44:17 +0000

    remove yanky put before/after highlight given keymap conflicts

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -18,8 +18,6 @@ return {
     { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
     { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
     { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
-    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
-    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
     { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
     { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
     { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
