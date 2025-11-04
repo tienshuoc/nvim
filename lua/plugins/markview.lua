0ab104394ee923f45e63453faff909d39ee8bf37
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2025-11-04 03:11:47 +0000

    make markview work with codecompanion

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,12 +1,29 @@
 return {
   "OXY2DEV/markview.nvim",
-  lazy = false,
+  lazy = true,
+  config = {
+    experimental = {
+      check_rtp = true,
+      check_rtp_msg = false,
+    },
+  },
   opts = {
     preview = {
       filetypes = { --[[ "markdown", ]]
         "codecompanion",
       }, -- Enable this just for  CodeCompanion. We use Markdown Preview for Md files.
       ignore_buftypes = {},
+
+      condition = function(buffer)
+        local ft, bt = vim.bo[buffer].ft, vim.bo[buffer].bt
+        if bt == "nofile" and ft == "codecompanion" then
+          return true
+        elseif bt == "nofile" then
+          return false
+        else
+          return true
+        end
+      end,
     },
   },
 }
