40d7f529ca1877f8ff8bda4ece6d914a1d44c87d
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2025-12-06 03:46:06 +0000

    allow lazy loading for diffview

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,6 +1,5 @@
 return {
-  "sindrets/diffview.nvim", -- Git diff page.
-  lazy = false, -- Requires Git >= 2.31.0 to work properly.
+  "sindrets/diffview.nvim", -- Git diff page. Requires Git >= 2.31.0 to work properly.
   keys = {
     { "<leader>gg", ":DiffviewOpen<CR>", mode = "n", { noremap = true, silent = true } },
   },
