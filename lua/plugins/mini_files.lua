9c50627aa5db730d84c6de636065fd70f694d7e4
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2025-12-06 04:37:18 +0000

    override the keymaps for mini files

--- lua/plugins/mini_files.lua
+++ lua/plugins/mini_files.lua
@@ -10,5 +10,21 @@ return {
       desc = "Open MiniFiles",
     },
   },
-  opts = {},
+  opts = {
+    mappings = {
+      close = "q",
+      go_in = "<Right>",
+      go_in_plus = "L",
+      go_out = "<Left>",
+      go_out_plus = "H",
+      mark_goto = "'",
+      mark_set = "m",
+      reset = "<BS>",
+      reveal_cwd = "@",
+      show_help = "g?",
+      synchronize = "=",
+      trim_left = "<",
+      trim_right = ">",
+    },
+  },
 }
