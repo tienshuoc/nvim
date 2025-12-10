86dcf1a9bbfc84cbdc09188f418940464c80f65e
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2025-12-10 04:00:59 +0000

    add lazy events so that DiffView opens when doing things like `git mergetool`

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,5 +1,6 @@
 return {
   "sindrets/diffview.nvim", -- Git diff page. Requires Git >= 2.31.0 to work properly.
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewRefresh" },
   keys = {
     { "<leader>gg", ":DiffviewOpen<CR>", mode = "n", { noremap = true, silent = true } },
   },
