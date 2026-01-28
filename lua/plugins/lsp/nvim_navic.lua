92fd78911f903ed7dd18f0b9e5884cf8525682b8
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2026-01-28 04:07:27 +0000

    Comments for nvim-navic

--- lua/plugins/lsp/nvim_navic.lua
+++ lua/plugins/lsp/nvim_navic.lua
@@ -10,6 +10,8 @@ return {
   config = function(_, opts)
     require("nvim-navic").setup(opts)
     -- Used to enable LSP symbol information on winbar
+
+    -- need to show per-buffer
     vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
   end,
 }
