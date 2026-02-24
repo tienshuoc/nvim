304a61a66633ed6beb686ce11daaa3fc47f18bf9
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2026-02-24 12:45:33 +0000

    Enable linediff keymap in visual mode

--- lua/plugins/linediff.lua
+++ lua/plugins/linediff.lua
@@ -3,8 +3,8 @@ return {
   keys = {
     {
       "<leader>ld",
-      "<Cmd>Linediff<CR>",
-      mode = "n",
+      ":Linediff<CR>",
+      mode = { "n", "v" },
       { noremap = true, silent = true, desc = "Linediff" },
     },
   },
