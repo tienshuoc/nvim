9a2375b0ff399d57f4c71f5c74dc520677d42f69
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2026-02-28 06:28:59 +0000

    Disable markdown preview autostart, otherwise DiffView.nvim crashes

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -6,7 +6,7 @@ return {
   build = "cd app && yarn install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
-    vim.g.mkdp_auto_start = 1 -- Open the preview window after entering the Markdown buffer.
+    vim.g.mkdp_auto_start = 0 -- Must be 0: auto_start=1 fires BufEnter autocommands that try to open a browser channel, crashing diffview.nvim when it opens markdown files.
     vim.g.mkdp_combine_preview = 1 -- Reuse previous opened preview window.
   end,
   ft = { "markdown" },
