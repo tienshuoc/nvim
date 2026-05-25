16a4146445a90438da46c1325a519e602b429bc3
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2026-05-25 17:14:55 +0000

    Consolidate mini.ai and treesitter_textobjects keymaps

--- lua/plugins/mini_ai.lua
+++ lua/plugins/mini_ai.lua
@@ -1,7 +1,21 @@
 return {
-  "echasnovski/mini.ai",
-  version = false,
+  "nvim-mini/mini.ai",
+  version = "*",
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
   config = function()
-    require("mini.ai").setup()
+    local spec_treesitter = require("mini.ai").gen_spec.treesitter
+    require("mini.ai").setup({
+      n_lines = 200, -- Number of lines within which textobject is searched
+      custom_textobjects = {
+        f = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+        c = spec_treesitter({ a = "@class.outer", i = "@class.inner" }),
+        p = spec_treesitter({ a = "@parameter.outer", i = "@parameter.inner" }),
+        m = spec_treesitter({ a = "@method.outer", i = "@method.inner" }),
+        o = spec_treesitter({
+          a = { "@conditional.outer", "@loop.outer" },
+          i = { "@conditional.inner", "@loop.inner" },
+        }),
+      },
+    })
   end,
 }
