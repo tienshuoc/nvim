eaa0f4a6e9b5bd56b3a82315adb51c9788488d10
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2026-09-16 23:45:42 +0000

    fix(ftplugin): restore TypeScript indentation on filetype changes

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -2,3 +2,7 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
+
+-- Restore defaults when this buffer changes filetype.
+local undo = "setlocal tabstop< softtabstop< shiftwidth< expandtab<"
+vim.b.undo_ftplugin = vim.b.undo_ftplugin and (vim.b.undo_ftplugin .. " | " .. undo) or undo
