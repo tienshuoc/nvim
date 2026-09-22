63cf5386425ebd67351a9b54cd887c7bef2180e8
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2026-09-22 00:10:00 +0000

    refactor(opencode): remove inactive Snacks integration

--- lua/plugins/opencode_nvim.lua
+++ lua/plugins/opencode_nvim.lua
@@ -1,32 +1,6 @@
 return {
   "nickjvandyke/opencode.nvim",
   version = "*", -- Latest stable release
-  dependencies = {
-    -- Also requires `lsof` command to be available
-    {
-      -- `snacks.nvim` integration is recommended, but optional
-      ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
-      "folke/snacks.nvim",
-      optional = true,
-      opts = {
-        input = {}, -- Enhances `ask()`
-        picker = { -- Enhances `select()`
-          actions = {
-            opencode_send = function(...)
-              return require("opencode").snacks_picker_send(...)
-            end,
-          },
-          win = {
-            input = {
-              keys = {
-                ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
