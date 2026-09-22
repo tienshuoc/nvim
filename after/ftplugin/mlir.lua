70908469bda411cff63de214d2fa1682f88fea9d
Tien-Shuo Chang <tienshuo.chang@gmail.com>
2026-09-22 22:53:42 +0000

    fix(mlir): undo local options when filetype changes

--- after/ftplugin/mlir.lua
+++ after/ftplugin/mlir.lua
@@ -1,1 +1,6 @@
+vim.opt_local.modifiable = false
 vim.opt_local.wrap = false
+
+-- Restore defaults when this buffer changes filetype.
+local undo = "setlocal modifiable< wrap<"
+vim.b.undo_ftplugin = vim.b.undo_ftplugin and (vim.b.undo_ftplugin .. " | " .. undo) or undo
